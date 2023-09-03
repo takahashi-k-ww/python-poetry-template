68f717e0846ea6fa52f7e9982c1a1570236a0907
Takahashi Katsuyuki <takahashi.k@world-wing.com>
2023-09-03 22:30:43 +0900

    fix .gitignore

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,14 +5,14 @@ import pytest
 from src.main import add
 
 
-def test_add():
+def test_add() -> None:
     assert add(1, 3) == 4
     assert add(-1, 3) == 2
     assert add(0, 0) == 0
     assert add(1, 0) == 1
 
 
-def test_add_with_str():
+def test_add_with_str() -> None:
     with pytest.raises(TypeError):
         add("1", "3")
 
